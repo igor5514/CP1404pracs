83454e4f3d044d9f9f3c351c7bef36ea6d3bb539
Igor Kenzin <62878129+igor5514@users.noreply.github.com>
2021-04-09 18:39:00 +0800

    09.04.2021

---
+++ prac3/temperatures.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+by Kenzin Igor
+"""
+
+def celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    return celsius * 9.0 / 5 + 32
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(input("Farenheit "))
+    return 5 / 9 * (fahrenheit - 32)
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    flag = True
+    while flag:
+        print(MENU)
+        choice = input(">>> ").upper()
+        if (choice == "Q" or choice == "q"):
+            flag = False
+        elif (choice == "C" or choice == "c"): #made it case insensitive
+            print("Result: {:.2f} F".format(celsius_to_fahrenheit()))
+        elif (choice == "F" or choice == "f"): # made it case insensitive
+            print("Result: {:.2f} C".format(fahrenheit_to_celsius()))
+        else:
+            print("Invalid option")
+    print("Thank you.")
+
+if __name__ == "__main__":
+    main()
