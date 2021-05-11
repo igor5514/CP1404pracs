b5ad4988f61f244b4865f13906bedb8999a63530
Igor Kenzin <62878129+igor5514@users.noreply.github.com>
2021-05-11 11:39:34 +0600

    Add files via upload

---
+++ prac7/squaring.py
@@ -0,0 +1,29 @@
+"""
+by Kenzin Igor
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+from kivy.properties import StringProperty
+
+__author__ = 'Kenzin Igor'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (640, 480)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        result = value ** 2
+        self.root.ids.output_label.text = str(result)
+
+
+SquareNumberApp().run()
