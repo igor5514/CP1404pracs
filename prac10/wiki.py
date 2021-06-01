c0b3814e256984a95b595e1e91cbbb85218bd027
Igor Kenzin <62878129+igor5514@users.noreply.github.com>
2021-06-01 09:41:29 +0600

    Update wiki.py

--- prac10/wiki.py
+++ prac10/wiki.py
@@ -1,6 +1,4 @@
-"""Create a new file called wiki.py and write a small script that prompts the user for a page title or search phrase
-, then prints the summary of that page.
-Use a simple loop that continues doing this until the user enters blank input.
+"""
 by Kenzin Igor
 """
 
